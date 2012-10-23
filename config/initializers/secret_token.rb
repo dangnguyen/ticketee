ecc72409612ec34d615ce564dec2dee6bb9a2fa6
Dang Nguyen <minhdang.net@gmail.com>
2012-10-23 11:41:57 +0700

    Generatet App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'a6db68367d5110955af761495e7c5fb6f027265cfb3f21851210783d79d5d806dbc38c35ebdede5de3c8528d55a96b4985959b010e2f07f3601abd738fc44302'
